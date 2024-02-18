5b7d34c91c14de26681712a6d4240288f6a54b20
Conner Vieira <cvieira@v0lttech.com>
2024-02-18 16:38:39 -0500

    Revised Tesseract CMake includes

--- src/cmake_modules/FindTesseract.cmake
+++ src/cmake_modules/FindTesseract.cmake
@@ -11,7 +11,7 @@ include(LibFindMacros)
 #libfind_pkg_check_modules(Tesseract_PKGCONF Tesseract)
 
 # Include dir
-find_path(Tesseract_INCLUDE_BASEAPI_DIR
+find_path(Tesseract_INCLUDE_DIR
   NAMES tesseract/baseapi.h
   HINTS "/usr/include"
         "/usr/include/tesseract"
@@ -22,40 +22,6 @@ find_path(Tesseract_INCLUDE_BASEAPI_DIR
         ${Tesseract_PKGCONF_INCLUDE_DIRS}
 		${CMAKE_SOURCE_DIR}/../libraries/tesseract-ocr/api/
 )
-find_path(Tesseract_INCLUDE_CCSTRUCT_DIR
-  NAMES publictypes.h
-  HINTS "/usr/include"
-        "/usr/include/tesseract"
-        "/usr/local/include"
-        "/usr/local/include/tesseract"
-        "/opt/local/include"
-        "/opt/local/include/tesseract"
-        ${Tesseract_PKGCONF_INCLUDE_DIRS}
-		${CMAKE_SOURCE_DIR}/../libraries/tesseract-ocr/ccstruct/
-)
-find_path(Tesseract_INCLUDE_CCMAIN_DIR
-  NAMES thresholder.h
-  HINTS "/usr/include"
-        "/usr/include/tesseract"
-        "/usr/local/include"
-        "/usr/local/include/tesseract"
-        "/opt/local/include"
-        "/opt/local/include/tesseract"
-        ${Tesseract_PKGCONF_INCLUDE_DIRS}
-		${CMAKE_SOURCE_DIR}/../libraries/tesseract-ocr/ccmain/
-)
-find_path(Tesseract_INCLUDE_CCUTIL_DIR
-  NAMES platform.h
-  HINTS "/usr/include"
-        "/usr/include/tesseract"
-        "/usr/local/include"
-        "/usr/local/include/tesseract"
-        "/opt/local/include"
-        "/opt/local/include/tesseract"
-        ${Tesseract_PKGCONF_INCLUDE_DIRS}
-		${CMAKE_SOURCE_DIR}/../libraries/tesseract-ocr/ccutil/
-)
-
 
 # Finally the library itself
 find_library(Tesseract_LIB
@@ -81,10 +47,7 @@ find_library(Leptonica_LIB
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
 set(Tesseract_PROCESS_INCLUDES 
-    Tesseract_INCLUDE_BASEAPI_DIR 
-	Tesseract_INCLUDE_CCSTRUCT_DIR
-	Tesseract_INCLUDE_CCMAIN_DIR
-	Tesseract_INCLUDE_CCUTIL_DIR
+    Tesseract_INCLUDE_DIR 
 	Tesseract_INCLUDE_DIRS)
 set(Tesseract_PROCESS_LIBS Tesseract_LIB Leptonica_LIB Tesseract_LIBRARIES)
 libfind_process(Tesseract)
